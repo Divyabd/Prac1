f5efcfea52f56c063049467044e9534dc25f19d5
Divyabd <divyagowda8393@gmail.com>
2021-02-05 14:03:28 +0530

    commit 4

--- S/src/s/D.java
+++ S/src/s/D.java
@@ -5,7 +5,10 @@ public class D {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello ppls");
-		
+	System.out.println("gello divya b d")	;
+	System.out.println("gello arjun")	;
+	
+	
 	}
 
 }
